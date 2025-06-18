012f302974b4161e3073a70823f7e51c248edb5d
René Rivas <rrivas@teravisiontech.com>
2025-06-18 13:17:51 -0400

    fh-react-gif-expert-app: app basis

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+const GifExpertApp = () => {
+  return (
+    <>
+      {/* Titulo */}
+      <h1>Gif Expert App!</h1>
+
+      {/* Buscador */}
+
+      {/* Listado Gifs */}
+    </>
+  );
+};
+
+export default GifExpertApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import GifExpertApp from "./GifExpertApp";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./styles.css"
+
+createRoot(document.getElementById("root")).render(<StrictMode>
+  <GifExpertApp />
+</StrictMode>);

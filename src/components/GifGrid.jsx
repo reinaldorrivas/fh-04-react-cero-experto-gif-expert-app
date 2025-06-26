ba3f79d0ee1cd60f8f49a28758ae50e98b08324b
Rene Rivas <rrivas@teravisiontech.com>
2025-06-26 14:12:10 -0400

    fh-react-gif-expert-app: Added useEffect to avoid unwanted getGif() re-renders and some template error handlers with the help of useState

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "./helpers/GetGifs";
+import GifItem from "./GifItem";
 
 const GifGrid = ({ category }) => {
+  const [gifs, setGifs] = useState(null);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
-    getGifs(category);
-  });
+    (async () => {
+      try {
+        const response = await getGifs(category);
+        setGifs(response);
+      } catch {
+        setError(true);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-      <h3>{category}</h3>
+      <h2>{category}</h2>
+      <div className="card-grid">
+        {gifs?.length === 0 ? (
+          <p>No se encontraron GIFs para esta categoría.</p>
+        ) : (
+          gifs?.map((image) => <GifItem key={image.id} {...image} />)
+        )}
+      </div>
+      {error && <p>Error al cargar los GIFs 😢</p>}
     </>
   );
 };

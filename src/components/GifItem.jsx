b53ebd05095ef360627a4cc27098c5b7108289de
Rene Rivas <rrivas@teravisiontech.com>
2025-06-26 15:52:23 -0400

    fh-react-gif-expert-app: Added GiftItem component

---
+++ src/components/GifItem.jsx
@@ -0,0 +1,10 @@
+const GifItem = ({ title, url }) => {
+  return (
+    <div className="card">
+      <img src={url} alt={title} />
+      <h3>{title || "Without Name"}</h3>
+    </div>
+  );
+};
+
+export default GifItem;

082f032e0311d4fe55bbbaac4a5e9e4f284665e1
Rene Rivas <rrivas@teravisiontech.com>
2025-07-08 13:08:03 -0400

    fh-react-gea: Added test for getGifs() helper.

---
+++ __tests__/features/GifExpertApp/components/GifGrid/GetGifs/GetGifs.test.ts
@@ -0,0 +1,20 @@
+import { getGifs } from "../../../../../../src/helpers/GetGifs";
+import { gifDataProps } from "../../../../../../src/types/GiphyTypes";
+
+describe("Testing GetGifs(): ", () => {
+  test("Debe retornar un arreglo de gifDataProps: ", async () => {
+    const queryTest = "Dragon Ball Z";
+    const parsedGifDataTest = {
+      id: expect.any(String),
+      title: expect.any(String),
+      url: expect.any(String),
+    };
+    const gifsData: gifDataProps[] = await getGifs(queryTest);
+
+    expect(gifsData.length).toBeGreaterThan(0);
+
+    gifsData.forEach((gifData) => {
+      expect(gifData).toStrictEqual(parsedGifDataTest);
+    });
+  });
+});
--- src/helpers/GetGifs.ts
+++ src/helpers/GetGifs.ts
@@ -1,13 +1,10 @@
 import type { gifDataProps, GiphyResponseProps } from "../types/GiphyTypes";
 
-
 export const getGifs = async (category: string): Promise<gifDataProps[]> => {
   let gifsData: gifDataProps[] = [];
 
   try {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${
-      import.meta.env.VITE_GIPHY_API_KEY
-    }&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${category}&limit=10`;
 
     const response = await fetch(url);
     const { data } = await response.json();
